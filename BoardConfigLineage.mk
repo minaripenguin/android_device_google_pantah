1f0c84e839e7990263a9b70717b3d20884d882a7
Alexander Koskovich <akoskovich@pm.me>
2024-05-09 12:41:00 +0800

    cheetah: Override rising properties
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,8 @@
 # SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
+
+BUILD_BROKEN_DUP_RULES := true
+DEVICE_PATH := device/google/pantah
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_cheetah
